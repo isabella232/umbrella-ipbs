50d742172c3621158a26032f103e7c6bc0180c38
Ben Keith <ben@benlk.com>
2017-08-01 17:19:02 -0400

    Add .entry-title class to post title on single view

--- wp-content/themes/ipbs/index.php
+++ wp-content/themes/ipbs/index.php
@@ -20,7 +20,7 @@ get_header();
             ?>
 
             <article>
-                <h1><?php the_title(); ?></h1>
+                <h1 class="entry-title"><?php the_title(); ?></h1>
                 <?php the_content(); ?>
             </article>
 
