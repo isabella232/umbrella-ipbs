4b18111e6ed9381857ed146a05f0267f93db7ec0
Ben Keith <ben@benlk.com>
2017-08-01 16:53:50 -0400

    Add post title to generic post template

--- wp-content/themes/ipbs/index.php
+++ wp-content/themes/ipbs/index.php
@@ -20,7 +20,8 @@ get_header();
             ?>
 
             <article>
-                <?php the_content() ?>
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>
             </article>
 
         <?php } ?>
