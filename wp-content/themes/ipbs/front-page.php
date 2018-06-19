355c8348b9c6243db9fc83fa07e4db5825cdd2fe
tylermachado <tyler@inn.org>
2018-06-19 13:25:14 -0400

    back to date, but ascending this time

--- wp-content/themes/ipbs/front-page.php
+++ wp-content/themes/ipbs/front-page.php
@@ -54,7 +54,7 @@ get_header();
                         'post_type'=>'station',
                         'posts_per_page'=>-1,
                         'posts_status'=>'publish',
-                        'orderby'=>'title',
+                        'orderby'=>'date',
                         'order'=>'ASC'
                     );
 
