fa1eb13ee0eb3f3f29bb521238621e2927c9cee0
Josh Darby <josh@inn.org>
2020-01-24 15:45:11 -0500

    Update largo-child-styles args

--- wp-content/themes/ipbs-largo/inc/enqueue.php
+++ wp-content/themes/ipbs-largo/inc/enqueue.php
@@ -6,6 +6,7 @@ function ipbs_enqueue_styles(){
 	wp_enqueue_style(
 		'largo-child-styles',
 		get_stylesheet_directory_uri() . '/css/child-style.css',
+		null,
 		filemtime( get_stylesheet_directory() . '/css/child-style.css' )
 	);
 	// fonts
