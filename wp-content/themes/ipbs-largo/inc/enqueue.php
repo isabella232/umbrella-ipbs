d9b401be14d739f076c9181b6833c2a6006488a3
Josh Darby <josh@inn.org>
2020-01-24 15:14:10 -0500

    Fonts, colors, typography styles, block color palette

--- wp-content/themes/ipbs-largo/inc/enqueue.php
+++ wp-content/themes/ipbs-largo/inc/enqueue.php
@@ -8,5 +8,14 @@ function ipbs_enqueue_styles(){
 		get_stylesheet_directory_uri() . '/css/child-style.css',
 		filemtime( get_stylesheet_directory() . '/css/child-style.css' )
 	);
+	// fonts
+	wp_enqueue_style(
+		'inter',
+		'https://rsms.me/inter/inter.css'
+	);
+	wp_enqueue_style(
+		'typekit',
+		'https://use.typekit.net/wzh8ith.css'
+	);
 }
 add_action( 'wp_enqueue_scripts', 'ipbs_enqueue_styles' );
