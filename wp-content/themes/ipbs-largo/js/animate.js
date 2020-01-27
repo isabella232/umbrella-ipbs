b790eadcf3505bfb42f457c67c5bacc70d149519
Ben Keith <ben@benlk.com>
2020-01-27 14:55:26 -0500

    Velocity is a method on jQuery, not a global var on its own.

--- wp-content/themes/ipbs-largo/js/animate.js
+++ wp-content/themes/ipbs-largo/js/animate.js
@@ -1,6 +1,5 @@
 var timeOut = [];
 var id = "";
-var $=jQuery;
 
 function opacityToZero() {
     var cities = document.querySelectorAll('#cities > g');
@@ -8,8 +7,8 @@ function opacityToZero() {
         var coverageGroups = cities[i].querySelectorAll('.coverage > g');
         var heading = cities[i].querySelector('.heading');
         var label = cities[i].querySelector('.label > g');
-        Velocity(heading, "stop");
-        Velocity(label, "stop");
+        jQuery.Velocity(heading, "stop");
+        jQuery.Velocity(label, "stop");
         heading.style.opacity = 0;
         label.style.opacity = 1;
         for (var q = 0; q < coverageGroups.length; q++) {
@@ -17,16 +16,16 @@ function opacityToZero() {
             var line = coverageGroups[q].querySelector('line');
             var text = coverageGroups[q].querySelector('text');
             if (circle) {
-                Velocity(circle, "stop");
+                jQuery.Velocity(circle, "stop");
                 circle.style.opacity = 0;
             }
             if (line) {
-                Velocity(line, "stop");
+                jQuery.Velocity(line, "stop");
                 line.style.opacity = 0;
                 line.setAttribute('stroke-dasharray', 1000);
             }
             if (text) {
-                Velocity(text, "stop");
+                jQuery.Velocity(text, "stop");
                 text.style.opacity = 0;
             }
         }
@@ -42,8 +41,8 @@ function animateCity(id) {
     var heading = clickedElement.querySelector('.heading');
     var label = clickedElement.querySelector('.label > g');
     var coverageGroup = clickedElement.querySelectorAll('.coverage > g');
-    Velocity(heading, {opacity: 1}, {duration: 500, delay: 200});
-    Velocity(label, {opacity: 0}, {duration: 500, delay: 200});
+    jQuery.Velocity(heading, {opacity: 1}, {duration: 500, delay: 200});
+    jQuery.Velocity(label, {opacity: 0}, {duration: 500, delay: 200});
 
     for (var w = 0; w < coverageGroup.length; w++) {
         (function (w) {
@@ -55,16 +54,16 @@ function animateCity(id) {
                     var radius = circle.getAttribute("r");
                     var opacity = circle.getAttribute("opacity");
                     circle.setAttribute("r", 0);
-                    Velocity(circle, {opacity: opacity}, 0);
-                    Velocity(circle, {r: radius}, {duration: 500, easing: "easeOutCirc"});
+                    jQuery.Velocity(circle, {opacity: opacity}, 0);
+                    jQuery.Velocity(circle, {r: radius}, {duration: 500, easing: "easeOutCirc"});
                 }
                 if (line) {
-                    Velocity(line, {opacity: opacity}, 0);
-                    Velocity(line, {'stroke-dashoffset': 500}, {duration: 0, delay: 0});
-                    Velocity(line, {'stroke-dashoffset': 0}, {duration: 500, delay: 100});
+                    jQuery.Velocity(line, {opacity: opacity}, 0);
+                    jQuery.Velocity(line, {'stroke-dashoffset': 500}, {duration: 0, delay: 0});
+                    jQuery.Velocity(line, {'stroke-dashoffset': 0}, {duration: 500, delay: 100});
                 }
                 if (text) {
-                    Velocity(text, {opacity: 1}, {duration: 500, delay: 100,});
+                    jQuery.Velocity(text, {opacity: 1}, {duration: 500, delay: 100,});
                 }
             }, 500 * w);
         }(w));
